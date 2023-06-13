c70564518c0b268bed0efe1767f3f2831b2625be
Stephen <37083603+st7ma784@users.noreply.github.com>
2023-06-14 00:17:58 +0100

    Update LakesModel.py

--- model/LakesModel.py
+++ model/LakesModel.py
@@ -120,7 +120,7 @@ class LightningCLIPModule(LightningModule):
         # print("input text",(text %self.vocab_size).shape)
 
         #+ (torch.randint_like(text,0,self.vocab_size,device=self.device)*mask),0,self.vocab_size)
-        x1 = self(torch.clamp(text,0,self.vocab_size), geoCode, Location)
+        x1 = self(text, geoCode, Location)
         x2 = self(torch.clamp(text+(torch.randint_like(text,0,self.vocab_size,device=self.device)*mask),0, self.vocab_size), geoCode, Location)
         print("x1",x1.shape)
         print("x2",x2.shape)
