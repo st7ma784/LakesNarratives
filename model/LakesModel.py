a49971fc9f41c8fb453b4fb7fec25baf7f43865a
Stephen <37083603+st7ma784@users.noreply.github.com>
2023-06-14 01:16:45 +0100

    Update LakesModel.py

--- model/LakesModel.py
+++ model/LakesModel.py
@@ -125,9 +125,13 @@ class LightningCLIPModule(LightningModule):
         x2 = x2 + (torch.randn_like(x2) * 0.05)
         x1 = x1 / x1.norm(dim=-1, keepdim=True)
         x2 = x2 / x2.norm(dim=-1, keepdim=True)
-
-        Lossx1=self.loss( x1 @ x2.T , torch.arange(x1.shape[0],device=self.device))
-        Lossx2=self.loss( x2 @ x1.T , torch.arange(x2.shape[0],device=self.device))
+        print("x1",x1.shape)
+        print("x2",x2.shape)
+        
+        l1= x1 @ x2.T *self.logit_scale.exp()
+        l2= x2 @ x1.T *self.logit_scale.exp()
+        Lossx1=self.loss( l1 , torch.arange(x1.shape[0],device=self.device))
+        Lossx2=self.loss( l2 , torch.arange(x2.shape[0],device=self.device))
         loss=Lossx1+Lossx2
         loss=loss/2
         self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
