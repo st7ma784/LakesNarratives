432ded2e1c7fd08732bb595a0631f156554b47ca
Stephen <37083603+st7ma784@users.noreply.github.com>
2023-06-14 01:05:55 +0100

    Update LakesModel.py

--- model/LakesModel.py
+++ model/LakesModel.py
@@ -89,8 +89,8 @@ class LightningCLIPModule(LightningModule):
 
         # print("Location(x)",x.shape)
         #make one hot of geoCode and Location by finding zeros and non zeros
-        one_hot_geoCode = geoCode.bool().type(self.dtype)
-        one_hot_Location = Location.bool().type(self.dtype)
+        one_hot_geoCode = geoCode.bool().type(self.dtype).unsqueeze(-1)
+        one_hot_Location = Location.bool().type(self.dtype).unsqueeze(-1)
         print("one_hot_geoCode",one_hot_geoCode.shape)
         print("one_hot_Location",one_hot_Location.shape)
         x = self.token_embedding(text).type(self.dtype) 
